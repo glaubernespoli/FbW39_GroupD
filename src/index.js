b7b52fffdbb6a780c7851ef64f4b6948738ad167
Sam Smith <sam@example.com>
2020-08-17 12:03:13 +0200

    js server added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+const server = express();
+
+server.use(express.static("/home/dci/Desktop/Github/Glauber_Tutoring/FbW39_GroupD/public", {
+"index": "index.html" }));
+
+const port = 2023;
+server.listen(port, function () {
+    console.log(`Server is listening to port: ${port}`);
+});
